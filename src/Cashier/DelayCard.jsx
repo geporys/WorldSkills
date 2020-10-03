42a005d1f96b6db3ffd512c2521a8ec2be3f8d98
German Ignatovich <ger266.u@yandex.ru>
2020-10-03 18:47:44 +0300

    Add first card

--- src/Cashier/DelayCard.jsx
+++ src/Cashier/DelayCard.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+  title: {
+    text: 'My chart',
+  },
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+};
 
 const useStyles = makeStyles({
-  root: {},
+  root: {
+    maxWidth: 500,
+  },
 });
 
 const DelayCard = () => {
+  const classes = useStyles();
+
   return (
-    <Card>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Задержка
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          Задержка
+        </Typography>
+        <HighchartsReact highcharts={Highcharts} options={options} />
+      </CardContent>
     </Card>
   );
 };
--- src/Cashier/index.jsx
+++ src/Cashier/index.jsx
@@ -3,14 +3,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import DelayCard from './DelayCard';
 
 const useStyles = makeStyles({
-  root: {},
+  root: {
+    padding: 16,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
 });
 
 const Cashier = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes}>
+    <div className={classes.root}>
       <DelayCard />
     </div>
   );
