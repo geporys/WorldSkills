b0de73e4c0bc1d39140dc09ddc8472ad971b4d0b
Okotie <Ottue@mail.ru>
2020-10-03 18:00:16 +0300

    add header

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyle = makeStyles({
+    img: {
+
+    },
+    img__logo: {
+        height: 69,
+        width: 88,
+    }
+});
 
 const Header = () => {
-  return <div />;
+    const classes = useStyle();
+  return (
+      <div className={classes.img}>
+        <div className={classes.img__logo}>
+            <img src="../img/logo.png" alt="  "/>
+        </div>
+      </div>
+  );
 };
 
 export default Header;
