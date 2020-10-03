e60d3e1f7ba1f8846204b67fbe634607dc781cf1
German Ignatovich <ger266.u@yandex.ru>
2020-10-03 17:23:12 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Header from './Header';
+import Routes from './Routes';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes />
+    </div>
+  );
+};
+
+export default App;
