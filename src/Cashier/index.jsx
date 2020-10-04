dca037dbd3278caf03e3dd40414492cdbbf48dd2
Okotie <Ottue@mail.ru>
2020-10-04 12:47:08 +0300

    add pie-diagram

--- src/Cashier/index.jsx
+++ src/Cashier/index.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import DelayCard from './DelayCard';
 import ErrorCard from './ErrorCard';
 import СheckCard from './СheckCard';
+import CountCard from "./CountCard";
 
 const useStyles = makeStyles({
   root: {
     padding: 16,
     display: 'flex',
   },
+    diagram: {
+      padding: 16,
+    }
 });
 
 const Cashier = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <СheckCard />
-      <ErrorCard />
-      <DelayCard />
-    </div>
+    <>
+        <div className={classes.root}>
+          <СheckCard />
+          <ErrorCard />
+          <DelayCard />
+        </div>
+        <div className={classes.diagram}>
+            <CountCard/>
+        </div>
+    </>
   );
 };
 
